955a665a72fe0fa61df62913c16a6946ecf98a81
jordann88 <108829476+jordann88@users.noreply.github.com>
2023-05-10 12:14:36 -0700

    added files

---
+++ app.js
@@ -0,0 +1,24 @@
+const recipes = [
+    {
+        "name": "recipe name",
+        "image": "",
+        "description": "short description of the recipe",
+        "source": "place where I got the recipe from",
+        "sourceURL": "URL of source",
+        "ingredients": [
+            "food1",
+            "food2",
+            "food3"
+        ],
+        "instructions": [
+            "step 1 is...", 
+            "step 2 is...",
+            "step 3 is..."
+        ],
+        "cuisine": "ex: korean, american, mexican",
+        "mealTime": "ex: breakfast vs. dinner vs. dessert",
+        "mealType": "bowl vs. salad vs. stirfry",
+        "temperature": "hot vs. cold",
+        "bulk": "is this good to make in bulk?"
+    }
+]
---
+++ todo.md
@@ -0,0 +1,4 @@
+# Project To-Dos:
+
+- 
+
